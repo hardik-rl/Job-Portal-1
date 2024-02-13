574fb946c4b572e3feacb1447ad55848dc36ab3b
hardik-rl <132266869+hardik-rl@users.noreply.github.com>
2024-02-13 12:27:08 +0530

    ui changes fix

--- my-react-app/src/module/job-details/JobPost.tsx
+++ my-react-app/src/module/job-details/JobPost.tsx
@@ -1,51 +1,30 @@
 import JobOverview from "./JobOverview";
 import JobPostDetails from "./JobPostDetails";
 
-const JobPost = ({jobData}: any) => {
-  console.log({jobData})
+const JobPost = ({ jobData }: any) => {
+  console.log({ jobData });
   return (
     <div className="job-post-company pt-120 pb-120">
       <div className="container">
         <div className="row justify-content-between">
           <div className="col-xl-7 col-lg-8">
-            <div className="single-job-items mb-50">
-              <div className="job-items">
-                <div className="company-img company-img-details">
-                  <a href="#">
-                    <img src="assets/img/icon/job-list1.png" alt="" />
-                  </a>
-                </div>
-                <div className="job-tittle">
-                  <a href="#">
-                    <h4>{jobData?.position.name}</h4>
-                  </a>
-                  <ul>
-                    <li>{jobData?.company_id?.name}</li>
-                    <li>
-                      <i className="fas fa-map-marker-alt"></i>{jobData?.job_location_id?.name}
-                    </li>
-                    <li>{jobData?.position?.salary_range}</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
             <div className="job-post-details">
               <div className="post-details1 mb-50">
                 <div className="small-section-tittle">
                   <h4>Job Description</h4>
                 </div>
-                <p>
-                  {jobData?.Description}
-                </p>
+                <p>{jobData?.description}</p>
               </div>
               <div className="post-details2  mb-50">
                 <div className="small-section-tittle">
                   <h4>Required Knowledge, Skills, and Abilities</h4>
                 </div>
                 <ul>
-                  {jobData?.knowledge_description?.map((knowledgeData:any, index: any) => (
-                    <li key={index}>{knowledgeData}</li>
-                  ))}
+                  {jobData?.knowledge_description?.map(
+                    (knowledgeData: any, index: any) => (
+                      <li key={index}>{knowledgeData}</li>
+                    )
+                  )}
                 </ul>
               </div>
               <div className="post-details2  mb-50">
@@ -53,16 +32,18 @@ const JobPost = ({jobData}: any) => {
                   <h4>Education + Experience</h4>
                 </div>
                 <ul>
-                  {jobData?.education_description?.map((educationData:any, index: any) => (
-                    <li key={index}>{educationData}</li>
-                  ))}
+                  {jobData?.education_description?.map(
+                    (educationData: any, index: any) => (
+                      <li key={index}>{educationData}</li>
+                    )
+                  )}
                 </ul>
               </div>
             </div>
           </div>
           <div className="col-xl-4 col-lg-4">
-            <JobOverview jobData={jobData}/>
-            <JobPostDetails jobData={jobData}/>
+            <JobOverview jobData={jobData} />
+            <JobPostDetails jobData={jobData} />
           </div>
         </div>
       </div>
--- my-react-app/src/module/job/SingleJobList.tsx
+++ my-react-app/src/module/job/SingleJobList.tsx
@@ -14,28 +14,20 @@ interface SingleJobProps {
 
 const SingleJobList = ({
   id,
-  img,
   title,
   location,
-  companyName,
   description,
 }: SingleJobProps) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <div className="single-job-items mb-30">
       <div className="job-items">
-        <div className="company-img">
-          <a href={`/job-details/${id}`}>
-            <img src={img} alt="" />
-          </a>
-        </div>
         <div className="job-tittle">
           <a href={`/job-details/${id}`}>
             <h4>{title}</h4>
           </a>
           <p>{description}</p>
           <ul>
-            <li>{companyName}</li>
             <li>
               <i className="fas fa-map-marker-alt"></i>
               {location}
