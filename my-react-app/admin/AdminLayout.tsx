c528586de1e533530be1dd9c169ccc06834a9679
hardik-rl <132266869+hardik-rl@users.noreply.github.com>
2024-02-16 17:47:49 +0530

    sidebar add

--- my-react-app/admin/AdminLayout.tsx
+++ my-react-app/admin/AdminLayout.tsx
@@ -1,16 +1,16 @@
 import "../assets/css/portal.css";
 
 import { Outlet } from "react-router-dom";
-import Footer from "./shared/Footer";
+// import Footer from "./shared/Footer";
 import Sidebar from "./shared/Sidebar";
 
 const AdminLayout = () => {
   return (
-    <>
+    <div className="app">
       <Outlet />
       <Sidebar />
-      <Footer />
-    </>
+      {/* <Footer /> */}
+    </div>
   );
 };
 
--- my-react-app/admin/shared/Sidebar.tsx
+++ my-react-app/admin/shared/Sidebar.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import logo from "../../assets/img/admin/grp-logo.png";
+import { CardList, Plus } from "../shared/Icon";
 
-const Sidebar = () => {
+type NavLinkType = {
+  href: string;
+  icon: JSX.Element;
+  text: string;
+};
+
+const NavLink = ({ href, icon, text }: NavLinkType) => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <li className="nav-item">
+      <a className="nav-link" href={href}>
+        <span className="nav-icon">{icon}</span>
+        <span className="nav-link-text">{text}</span>
+      </a>
+    </li>
+  );
+};
 
-export default Sidebar
+const AppNav = () => {
+  return (
+    <nav className="app-nav app-nav-main flex-grow-1">
+      <ul className="app-menu list-unstyled accordion" id="menu-accordion">
+        <NavLink href="#0" text="Candidates List" icon={<CardList />} />
+        <NavLink href="#0" text="Create New Job" icon={<Plus />} />
+      </ul>
+    </nav>
+  );
+};
+
+const SideBar = () => {
+  return (
+    <div id="app-sidepanel" className="app-sidepanel sidepanel-visible">
+      <div id="sidepanel-drop" className="sidepanel-drop"></div>
+      <div className="sidepanel-inner d-flex flex-column">
+        <a href="#" id="sidepanel-close" className="sidepanel-close d-xl-none">
+          ×
+        </a>
+        <div className="app-branding">
+          <a className="app-logo" href="index.html">
+            <img className="logo-icon me-2" src={logo} alt="logo" />
+          </a>
+        </div>
+        <AppNav />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
