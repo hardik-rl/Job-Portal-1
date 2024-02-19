0b9379a63de3d7175939f1596cc72dc597fe0eb1
Ronak-Navadia <navadiaronak@gmail.com>
2024-02-20 01:40:06 +0530

    add all pending apis
    add all pending apis

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Job-Portal
+
+# How to Set Up Backend
+
+## Step 1: Set up MongoDB
+
+## Step 2: npm install
+
+## Step 3: Setup Environment Variables
+
+Set the following environment variables:
+- MONGODB_URI=mongodb://localhost:27017/job-portal
+- PORT=3000
+- JWT_SECRET=secret
+
+## Step 4: Run the following commands
+```bash
+npm run seed:job-category
+npm run seed:job-location
+npm run seed:admin
+npm run start
--- backend/seeder/CompanySeeder.js
+++ backend/seeder/CompanySeeder.js
@@ -1,49 +1,49 @@
-// seed.js
-const Company = require("../models/CompanyModel");
-const connectDb = require("../config/dbConnection");
+// // seed.js
+// const Company = require("../models/CompanyModel");
+// const connectDb = require("../config/dbConnection");
 
-connectDb();
+// connectDb();
 
-// Insert Company static data
-try {
-  Company.insertMany([
-    {
-      name: "Company1",
-      logo_url: "https://picsum.photos/100/100",
-      description: "Company1 description",
-      website: "https://www.company1.com",
-      email_id: "company1@company.com",
-    },
-    {
-      name: "Company2",
-      logo_url: "https://picsum.photos/100/100",
-      description: "Company2 description",
-      website: "https://www.company2.com",
-      email_id: "company2@company.com",
-    },
-    {
-      name: "Company3",
-      logo_url: "https://picsum.photos/100/100",
-      description: "Company3 description",
-      website: "https://www.company3.com",
-      email_id: "company3@company.com",
-    },
-    {
-      name: "Company4",
-      logo_url: "https://picsum.photos/100/100",
-      description: "Company4 description",
-      website: "https://www.company4.com",
-      email_id: "company4@company.com",
-    },
-    {
-      name: "Company5",
-      logo_url: "https://picsum.photos/100/100",
-      description: "Company5 description",
-      website: "https://www.company5.com",
-      email_id: "company5@company.com",
-    },
-  ]);
-  console.log("Static data inserted successfully");
-} catch (error) {
-  console.error("Error inserting static data:", error);
-}
+// // Insert Company static data
+// try {
+//   Company.insertMany([
+//     {
+//       name: "Company1",
+//       logo_url: "https://picsum.photos/100/100",
+//       description: "Company1 description",
+//       website: "https://www.company1.com",
+//       email_id: "company1@company.com",
+//     },
+//     {
+//       name: "Company2",
+//       logo_url: "https://picsum.photos/100/100",
+//       description: "Company2 description",
+//       website: "https://www.company2.com",
+//       email_id: "company2@company.com",
+//     },
+//     {
+//       name: "Company3",
+//       logo_url: "https://picsum.photos/100/100",
+//       description: "Company3 description",
+//       website: "https://www.company3.com",
+//       email_id: "company3@company.com",
+//     },
+//     {
+//       name: "Company4",
+//       logo_url: "https://picsum.photos/100/100",
+//       description: "Company4 description",
+//       website: "https://www.company4.com",
+//       email_id: "company4@company.com",
+//     },
+//     {
+//       name: "Company5",
+//       logo_url: "https://picsum.photos/100/100",
+//       description: "Company5 description",
+//       website: "https://www.company5.com",
+//       email_id: "company5@company.com",
+//     },
+//   ]);
+//   console.log("Static data inserted successfully");
+// } catch (error) {
+//   console.error("Error inserting static data:", error);
+// }
