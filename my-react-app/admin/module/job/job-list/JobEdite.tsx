ac7a0f2ebdcb7e658f9e7f4478ccff5a923f157e
hardik-rl <132266869+hardik-rl@users.noreply.github.com>
2024-02-20 10:25:53 +0530

    edit and delete job modal design add

---
+++ my-react-app/admin/module/job/job-list/JobEdite.tsx
@@ -0,0 +1,230 @@
+
+import FormControl from "../../../../src/components/FormControl";
+import FormLabel from "../../../../src/components/FormLabel";
+
+const JobEdite = ({showModal, setShowModal}:any) => {
+  return (
+    <div>
+      {showModal && (
+        <>
+          <div
+            className="modal fade modal-toggle show"
+            id="exampleModal"
+            data-backdrop="static"
+            data-keyboard="false"
+            tabIndex={-1}
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-lg">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">
+                    Accounts & Finance
+                  </h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    onClick={()=> setShowModal(false)}
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <form>
+                    <div className="form-row">
+                      <div className="form-group col-md-4">
+                        <FormLabel name="First Name" htmlFor="htmlFor" />
+                        <FormControl
+                          onChange={()=>{}}
+                          // value={formik.values.first_name}
+                          id="first_name"
+                          value="first name"
+                          type="text"
+                          name="first_name"
+                        />
+                      </div>
+                      <div className="form-group col-md-4">
+                        <FormLabel name="Last Name" htmlFor="lastname" />
+                        <FormControl
+                          onChange={()=>{}}
+                          // value={formik.values.last_name}
+                          id="last_name"
+                          type="text"
+                          value="last name"
+                          name="last_name"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel name="Email" htmlFor="youremail" />
+                        <FormControl
+                          onChange={()=>{}}
+                          // value={formik.values.email}
+                          id="email"
+                          type="text"
+                          value="email"
+                          name="email"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel name="Pancard number" htmlFor="pan_number" />
+                        <FormControl
+                          onChange={(event: any) => handleOnChangeEvent(event)}
+                          // value={formik.values.pan_number}
+                          id="pan_number"
+                          type="text"
+                          value="pan"
+                          name="pan_number"
+                        />
+                      </div>
+                      <div className="form-group col-md-4">
+                        <FormLabel
+                          name="Mobile number"
+                          htmlFor="mobilenumber"
+                        />
+                        <FormControl
+                          onChange={(event: any) => handleOnChangeEvent(event)}
+                          // value={formik.values.mobile_number}
+                          id="mobile_number"
+                          type="number"
+                          value="mobile_number"
+                          name="mobile_number"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel name="Education" htmlFor="education" />
+                        <FormControl
+                          onChange={()=>{}}
+                          // value={formik.values.education}
+                          id="education"
+                          type="text"
+                          value="education"
+                          name="education"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <label htmlFor="ctc">CTC (In Laksh)</label>
+                        <FormLabel name="" htmlFor="ctc" />
+                        <FormControl
+                         onChange={()=>{}}
+                          // onChange={(event: any) => handleOnChangeEvent(event)}
+                          // value={formik.values.ctc}
+                          id="ctc"
+                          type="number"
+                          name="ctc"
+                          value="ctc"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel name="Expected CTC" htmlFor="expctc" />
+                        <FormControl
+                         onChange={()=>{}}
+                          // onChange={(event: any) => handleOnChangeEvent(event)}
+                          // value={formik.values.expected_ctc}
+                          id="expctc"
+                          type="number"
+                          value="expected_ctc"
+                          name="expected_ctc"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel
+                          name="Notice Period"
+                          htmlFor="notice_period"
+                        />
+                        <FormControl
+                         onChange={()=>{}}
+                          // onChange={(event: any) => handleOnChangeEvent(event)}
+                          // value={formik.values.notice_period}
+                          id="notice_period"
+                          value="notice period"
+                          type="text"
+                          name="notice_period"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel
+                          name="Total Work Experience"
+                          htmlFor="workexperience"
+                        />
+                        <FormControl
+                         onChange={()=>{}}
+                          // onChange={(event: any) => handleOnChangeEvent(event)}
+                          // value={formik.values.total_work_experience}
+                          id="workexperience"
+                          type="text"
+                          value="workexp"
+                          name="total_work_experience"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <FormLabel name="Gender" htmlFor="gendar" />
+                        <FormControl
+                          onChange={()=>{}}
+                          value="male"
+                          // value={formik.values.gender}
+                          id="gendar"
+                          type="text"
+                          name="gender"
+                        />
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <label htmlFor="inputState">State</label>
+                        <select
+                          className="form-control"
+                          id="inputState"
+                          // value={formik.values.state}
+                          name="state"
+                          // onChange={handleSelectOnChangeEvent}
+                        >
+                          <option>State</option>
+                          {/* {statesDataList.map((state) => (
+                            <option key={state.value} value={state.value}>
+                              {state.label}
+                            </option>
+                          ))} */}
+                        </select>
+                      </div>
+
+                      <div className="form-group col-md-4">
+                        <label htmlFor="resume">Add Resume</label>
+                        <input
+                          type="file"
+                          className="form-control"
+                          id="resume"
+                        />
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="submit"
+                    className="btn-primary px-2 py-1 border-0"
+                    // onClick={handleSubmitEvent}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="modal-backdrop fade show"></div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default JobEdite;
--- my-react-app/admin/module/job/job-list/index.tsx
+++ my-react-app/admin/module/job/job-list/index.tsx
@@ -1,60 +1,76 @@
-import { Link } from "react-router-dom";
 import { getAllJob } from "../api";
 import { useQuery } from "@tanstack/react-query";
+import JobEdite from "../job-list/JobEdite";
+import JobDelete from "../job-list/JobDelete";
+
+import { useState } from "react";
 
 const Create = () => {
   const { data } = useQuery(["getAllJob"], getAllJob);
   const jobListData = data;
-  return (
-    <div className="app-wrapper">
-      <div className="app-content pt-3 p-md-3 p-lg-4">
-        <div className="container-xl">
-          <div className="row g-3 mb-4 align-items-center justify-content-between">
-            <div className="col-auto">
-              <h1 className="app-page-title mb-0">All Jobs</h1>
-            </div>
-          </div>
+  const [showModal, setShowModal] = useState(false);
+  const [deleteModal, setDeleteModal] = useState(false);
 
-          <div className="g-4 mb-4">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Company Name</th>
-                  <th scope="col">Company Email</th>
-                  <th scope="col">Company Description</th>
-                  <th scope="col">Education Description</th>
-                  <th scope="col">Job Nature</th>
-                  <th scope="col" colSpan={2}>
-                    Vacancy
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {jobListData &&
-                  jobListData.map((item: any, index: number) => (
-                    <tr key={index}>
-                      <td>{item.company_name}</td>
-                      <td>{item.company_email}</td>
-                      <td>{item.company_description}</td>
-                      <td>{item.education_description}</td>
-                      <td>{item.nature}</td>
-                      <td>{item.vacancy}</td>
-                      <td>
-                        <Link to="#0" className="ml-2 btn-primary p-2">
-                          Edit
-                        </Link>
-                        <Link to="#0" className="ml-2 btn-danger p-2">
-                          Delete
-                        </Link>
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+  return (
+    <>
+      <div className="app-wrapper">
+        <div className="app-content pt-3 p-md-3 p-lg-4">
+          <div className="container-xl">
+            <div className="row g-3 mb-4 align-items-center justify-content-between">
+              <div className="col-auto">
+                <h1 className="app-page-title mb-0">All Jobs</h1>
+              </div>
+            </div>
+
+            <div className="g-4 mb-4">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Company Name</th>
+                    <th scope="col">Company Email</th>
+                    <th scope="col">Company Description</th>
+                    <th scope="col">Education Description</th>
+                    <th scope="col">Job Nature</th>
+                    <th scope="col" colSpan={2}>
+                      Vacancy
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {jobListData &&
+                    jobListData.map((item: any, index: number) => (
+                      <tr key={index}>
+                        <td>{item.company_name}</td>
+                        <td>{item.company_email}</td>
+                        <td>{item.company_description}</td>
+                        <td>{item.education_description}</td>
+                        <td>{item.nature}</td>
+                        <td>{item.vacancy}</td>
+                        <td className="d-flex">
+                          <button
+                            className="ml-2 btn-primary p-2"
+                            onClick={() => setShowModal(true)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="ml-2 btn-danger p-2"
+                            onClick={() => setDeleteModal(true)}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <JobEdite setShowModal={setShowModal} showModal={showModal} />
+      <JobDelete setDeleteModal={setDeleteModal} deleteModal={deleteModal} />
+    </>
   );
 };
 
