ee869a5640fb06f9c1afe55897d5ca914017f15f
hardik-rl <132266869+hardik-rl@users.noreply.github.com>
2024-03-09 12:17:46 +0530

    pan card validation add

--- my-react-app/src/module/job/validation.ts
+++ my-react-app/src/module/job/validation.ts
@@ -4,7 +4,9 @@ export const ApplicationModalSchema = Yup.object().shape({
   first_name: Yup.string().required('First name is required'),
   last_name: Yup.string().required('Last name is required'),
   email: Yup.string().email('Invalid email address').required('Email is required'),
-  pan_number: Yup.string().required('PAN number is required'),
+  pan_number: Yup.string()
+  .matches(/[a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}/, 'Please Enter a Valid PAN Number')
+  .required('PAN Number is Required'),
   mobile_number: Yup.string().required('Mobile number is required'),
   education: Yup.string().required('Education details are required'),
   ctc: Yup.string().required('Current CTC is required'),
