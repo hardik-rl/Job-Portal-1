3bcff7608d497d2c0ebe5b5e9109582f3826dfef
hardik-rl <132266869+hardik-rl@users.noreply.github.com>
2024-03-09 14:04:23 +0530

    email validation

--- my-react-app/src/module/job/validation.ts
+++ my-react-app/src/module/job/validation.ts
@@ -3,13 +3,12 @@ import * as Yup from 'yup';
 export const ApplicationModalSchema = Yup.object().shape({
   first_name: Yup.string().required('First name is required'),
   last_name: Yup.string().required('Last name is required'),
-  email: Yup.string().email('Invalid email address').required('Email is required'),
+  email: Yup.string().email().matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, 'Please Enter a Valid Email Address').required('Email is required'),
   pan_number: Yup.string()
   .matches(/[a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}/, 'Please Enter a Valid PAN Number')
   .required('PAN Number is Required'),
   mobile_number: Yup.string()
-  .matches(/^[0-9]{10}$/, 'Please Enter a Valid 10-Digit Mobile Number')
-  .required('Mobile Number is Required'),
+  .matches(/^[0-9]{10}$/, 'Please Enter a Valid 10-Digit Mobile Number').required('Mobile Number is Required'),
   education: Yup.string().required('Education details are required'),
   ctc: Yup.string().required('Current CTC is required'),
   expected_ctc: Yup.string().required('Expected CTC is required'),
